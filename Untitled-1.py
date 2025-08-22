737cd8d65417e50f4accb7764c7261080260f511
Davi Anderson <davi290509@gmail.com>
2025-08-22 08:50:53 -0300

    Add files via upload

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,55 +1,19 @@
-class animal:
-def __init__ (self, name, sound) :
-    self.name = name
-    self.sound = sound
-    def fazer_som (self)
-        exibe qual som o animal fazer_som
+def classificar_idade ():
+  try:
+    idade = int(input("Digite sua idade :"))
 
-        print (f"{self.name}  faz: {self.sound} ")
+    if idade < 0:
+      print ("Idade não pode ser negativa!")
+    elif idade < 13:
+      print ("voce é uma criança.")
+    elif idade < 18:
+      print ("voce é um adolescente.")
+    elif idade < 65:
+      print ("voce é um adulto.")
+    elif idade < 90:
+      print ("você é um idoso.")
 
-        class cachorro (Animal)
-            
-            sub classe que representa um cachorro 
-            atributos
-            name(str) : nome do cachorro 
-            sound (str): som fixo "au au "
+execept ValueError:
 
-            def __init__ (self, name) :
+Print ("por favor, digite um numero valido ")
 
-                inicializa um cachorro com um nome pre definido 
-
-                parametros 
-
-                name (str): nome do cachorro 
-
-                super(). __init__(name, sound = "au au") #chama o construtor da classe pai com som especifico 
-
-                class Gato (animal):
-                    subclasse que representa um gato 
-
-                    atributos
-
-                    name (str): nome do gato 
-                    sound (str): som fixo "miau" (sobrescrito base da classe).
-
-                    def __init__(self, name):
-
-                        parametros
-            nome(str): nome do gato.
-
-            super().__init__(name, sound="Miau!")  # Chama o construtor da classe pai com som específico
-
-
-# --- TESTE DAS CLASSES ---
-if __name__ == "__main__":
-    """
-    Bloco de teste para verificar o funcionamento das classes.
-    """
-    rex = Cachorro("Rex")  # Cria um cachorro chamado Rex
-    whiskers = Gato("Whiskers")  # Cria um gato chamado Whiskers
-
-    rex.fazer_som()  # Saída esperada: "Rex faz: Au Au!"
-    whiskers.fazer_som()  # Saída esperada: "Whiskers faz: Miau!"
-
-
-                
